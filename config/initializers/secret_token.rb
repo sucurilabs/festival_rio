59b91d2ceac13a935ad7080015265eba4ac4d1aa
yurimello <yuri.mcr@gmail.com>
2012-09-25 17:30:21 -0300

    Initializing project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoteiroCultural::Application.config.secret_token = '206053567e6915d7a664ffb55b42b90aab23f1fed2d4d711eaedacd22e416e6e59f804dfdc6867e31769057c87f4bddeacbc40e6450430a3976bd74e18d98922'
